6220c4c90c74a698a505f9cf2ef8ea47c3427584
mzhao15 <cliffish0408@gmail.com>
2019-07-06 16:01:33 -0400

    'add a sql file'

---
+++ sample.sql
@@ -0,0 +1,5 @@
+
+SELECT * FROM mytable1;
+
+SELECT *
+FROM mytable2
--- script.py
+++ script.py
@@ -1,4 +1,14 @@
 import sys
 
-for i in range(10):
-    print(i)
+
+with open("sample.sql", "r") as f:
+    sqls = f.read()
+    for sql in sqls.split(";"):
+        sqlcommand = sql.strip()
+        print(sqlcommand)
+        # print(len(sqlcommand))
+
+my_str = "this is \n the"
+new_str = my_str.replace("\n ", "")
+print(new_str)
+
