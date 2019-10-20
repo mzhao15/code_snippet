1df0856287b583e77b99497c643726c39caf6b24
mzhao15 <cliffish0408@gmail.com>
2019-10-20 09:26:36 -0400

    work on 10202019

--- script.py
+++ script.py
@@ -12,3 +12,8 @@ my_str = "this is \n the"
 new_str = my_str.replace("\n ", "")
 print(new_str)
 
+
+my_dict = {"b": "name", "a": "age"}
+my_list = ["name", "age"]
+sorted_dict = sorted(my_dict, key=my_dict.__getitem__)
+print(sorted_dict)
